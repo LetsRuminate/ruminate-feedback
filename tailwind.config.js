790f14e24d23c10ffd79210d98d59a179ff59f6c
John Jang <jscolbe9675@gmail.com>
2023-11-01 22:15:26 -0400

    initial set up

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", //
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "brand-black": "#1f271b",
+        "brand-blue": "#02819e",
+        "brand-brown": "#38342a",
+        "brand-green": "#0f7b41",
+        "brand-yellow": "#ffba49",
+      },
+    },
+  },
+  plugins: [],
+};
