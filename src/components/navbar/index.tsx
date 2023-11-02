8560dffb38a6810701d4d59eaa91a3a2e721f900
John Jang <jscolbe9675@gmail.com>
2023-11-01 22:28:56 -0400

    making changes in the navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,26 +1,9 @@
- 
-import { Link } from "react-router-dom";
 import brandLogo from "../../assets/images/brand-logo.svg";
 
 export default function NavBar() {
   return (
-    <nav className="bg-brand-green px-[72px] py-[30px] flex justify-between">
-      <div>
-        <Link to='/'>
+    <nav className="bg-brand-green px-[72px] py-[30px] ">
       <img alt="Feedback logo" src={brandLogo} />
-      </Link>
-      </div>
-      <div className="flex gap-5">
-        <div>
-        <Link to='/admin' className="text-white">Admin</Link>
-        </div>
-        <div>
-        <Link to='/producer' className="text-white">Producer</Link>
-        </div>
-        <div>
-        <Link to='/evaluator' className="text-white">Evaluator</Link>
-        </div>
-      </div>
-    </nav>);
- 
+    </nav>
+  );
 }
