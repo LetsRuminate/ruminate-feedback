c4694c6124a2ed707a98cc7691c6181bc9271b52
John Jang <jscolbe9675@gmail.com>
2023-11-05 12:02:54 -0500

    forgot footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./components/footer";
 import NavBar from "./components/navbar";
 import { Outlet } from "react-router-dom";
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <NavBar />
       <Outlet />
+      <Footer />
     </>
   );
 }
