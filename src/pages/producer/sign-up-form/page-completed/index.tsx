c317da70dfc84c1e68caed06ab5c8648679420d0
John Jang <jscolbe9675@gmail.com>
2023-11-24 19:48:37 -0500

    minor UI bug fix

--- src/pages/producer/sign-up-form/page-completed/index.tsx
+++ src/pages/producer/sign-up-form/page-completed/index.tsx
@@ -1,6 +1,8 @@
 import LastStep from '@assets/registration/progress-bar-1/step5.svg'
 import FinalStep from "@assets/registration/progress-bar-2/page-5"
 
+import Underline from '@assets/registration/progress-underline/underline.svg'
+
 import { Link } from 'react-router-dom';
 
 export default function ProducerCompleted() {
@@ -17,8 +19,11 @@ export default function ProducerCompleted() {
             <p className='text-white text-sm font-manrope font-normal pl-2'>How You Found Us</p>
             <p className='text-white text-sm font-manrope font-normal -mr-2'>Completed</p>
             </div>
+            <div className="flex justify-end mr-28">
+            <img src={Underline} alt="Underline" width={98}/>
+            </div>
         </div>
-        <div className='border border-white mx-auto w-[874px] px-14 py-20'>
+        <div className='border border-white mx-auto w-[874px] px-14 py-14'>
            <h1 className='text-white text-2xl font-manrope font-normal'>Congratulations!</h1>
            <p className='text-white text-base font-manrope font-medium pt-6 pb-12'>
            You just signed up for an evaluation with Feedback! As you are reading this, we are reaching out to evaluators. Once evaluators have signed on to your evaluation, we’ll post their shipping addresses (and your next steps) in your dashboard.<br/><br/>Click below to access your Producer Dashboard!
