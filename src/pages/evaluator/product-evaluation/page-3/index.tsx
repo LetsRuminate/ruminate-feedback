890740be841fd67486b937fd2248722b092aad18
John Jang <jscolbe9675@gmail.com>
2023-11-29 15:58:15 -0500

    re-structured the image files and importing style for all the forms

--- src/pages/evaluator/product-evaluation/page-3/index.tsx
+++ src/pages/evaluator/product-evaluation/page-3/index.tsx
@@ -1,23 +1,57 @@
-import StepThree from '@assets/registration/progress-bar-1/step3.svg'
-import ThirdStep from "@assets/registration/progress-bar-2/page-3"
+// Default Stage
+// The Sandwich
+import StepOneActive from '@assets/registration/progress-bar-1/step-1/step-1-active.svg'
+import StepTwoActive from '@assets/registration/progress-bar-1/step-2/step-2-active.svg'
+import StepThreeActive from '@assets/registration/progress-bar-1/step-3/step-3-active.svg'
+import StepFourInactive from '@assets/registration/progress-bar-1/step-4/step-4-inactive.svg'
+import StepFiveInactive from '@assets/registration/progress-bar-1/step-5/step-5-inactive.svg'
+// The Number Progress
+import PageOneActive from '@assets/registration/progress-bar-2/step-1/step-1-active.svg'
+import PageTwoActive from '@assets/registration/progress-bar-2/step-2/step-2-active.svg'
+import PageThreeActive from '@assets/registration/progress-bar-2/step-3/step-3-active.svg'
+import PageFourInactive from '@assets/registration/progress-bar-2/step-4/step-4-inactive.svg'
+import PageFiveInactive from '@assets/registration/progress-bar-2/step-5/step-5-inactive.svg'
+
+import Connector from '@assets/registration/progress-bar-2/progress-bar-connector/inactive.svg'
+import ConnectorActive from '@assets/registration/progress-bar-2/progress-bar-connector/active.svg'
+
+import Underline from '@assets/registration/progress-underline/underline.svg'
 
 import { Link } from 'react-router-dom';
 
 export default function EvaluationPage3() {
     return (
         <div className='py-20 bg-[#345EC9]'>
-        <div className='border border-white w-[874px] py-8 mx-auto'>
-            <h1 className='text-white text-5xl font-manrope font-bold text-center'>PRODUCT EVALUATION FORM</h1>
-            <img src={StepThree} alt="Default Progress Bar" width={648} className='mx-auto' />
-            <ThirdStep/>
-            <div className='flex justify-between px-32'>
-            <p className='text-white text-sm font-manrope font-normal'>Personal Info</p>
-            <p className='text-white text-sm font-manrope font-normal -ml-4'>Unboxing</p>
-            <p className='text-white text-sm font-manrope font-normal'>Senses</p>
-            <p className='text-[#ffffff80] text-sm font-manrope font-normal ml-5'>Overall</p>
-            <p className='text-[#ffffff80] text-sm font-manrope font-normal'>Completed</p>
+            <div className='border border-white w-[874px] py-8 mx-auto'>
+                <h1 className='text-white text-5xl font-manrope font-bold text-center'>PRODUCT EVALUATION FORM</h1>
+                <div className='flex justify-between px-32 items-end py-7'>
+                    <img src={StepOneActive} alt="Step One Active" />
+                    <img src={StepTwoActive} alt="Step Two Active" />
+                    <img src={StepThreeActive} alt="Step Three Active" />
+                    <img src={StepFourInactive} alt="Step Four Inactive" />
+                    <img src={StepFiveInactive} alt="Step Five Inactive" />
+                </div>
+                {/* the below is for the number progress bar */}
+                <div className='flex justify-center pb-3'>
+                    <img src={PageOneActive} alt="Page One Active" />
+                    <img src={ConnectorActive} alt="Connector" />
+                    <img src={PageTwoActive} alt="Page Two Active" />
+                    <img src={ConnectorActive} alt="Connector" />
+                    <img src={PageThreeActive} alt="Page Three Active" />
+                    <img src={Connector} alt="Connector" />
+                    <img src={PageFourInactive} alt="Page Four Inactive" />
+                    <img src={Connector} alt="Connector" />
+                    <img src={PageFiveInactive} alt="Page Five Inactive" />
+                </div>
+                <div className='flex justify-between px-28'>
+                <p className='text-white text-sm font-manrope font-normal'>Personal Info</p>
+                <p className='text-white text-sm font-manrope font-normal -ml-3'>Unboxing</p>
+                <p className='text-white text-sm font-manrope font-normal -ml-3'>Senses</p>
+                <p className='text-[#ffffff80] text-sm font-manrope font-normal'>Overall</p>
+                <p className='text-[#ffffff80] text-sm font-manrope font-normal'>Completed</p>
+                </div>
+                <img src={Underline} alt="Underline" width={98} className='mx-auto'/>
             </div>
-        </div>
         <div className='border border-white mx-auto w-[874px] px-14 py-20'>
             <button className='text-white text-base font-manrope font-medium rounded-3xl border border-white px-9 py-3 mr-5'><Link to='/evaluation-page-2'>Previous</Link></button>
             <button className='text-[#345EC9] text-base font-manrope font-semibold bg-white px-11 py-3 rounded-3xl'><Link to='/evaluation-page-4'>Next</Link></button>
--- src/pages/producer/product-intake/default-page/index.tsx
+++ src/pages/producer/product-intake/default-page/index.tsx
@@ -1,5 +1,18 @@
-import Default from '@assets/registration/progress-bar-1/default.svg'
-import DefaultStep from '@assets/registration/progress-bar-2/default-page';
+// Default Stage
+// The Sandwich
+import StepOneInactive from '@assets/registration/progress-bar-1/step-1/step-1-inactive.svg'
+import StepTwoInactive from '@assets/registration/progress-bar-1/step-2/step-2-inactive.svg'
+import StepThreeInactive from '@assets/registration/progress-bar-1/step-3/step-3-inactive.svg'
+import StepFourInactive from '@assets/registration/progress-bar-1/step-4/step-4-inactive.svg'
+import StepFiveInactive from '@assets/registration/progress-bar-1/step-5/step-5-inactive.svg'
+// The Number Progress
+import PageOneInactive from '@assets/registration/progress-bar-2/step-1/step-1-inactive.svg'
+import PageTwoInactive from '@assets/registration/progress-bar-2/step-2/step-2-inactive.svg'
+import PageThreeInactive from '@assets/registration/progress-bar-2/step-3/step-3-inactive.svg'
+import PageFourInactive from '@assets/registration/progress-bar-2/step-4/step-4-inactive.svg'
+import PageFiveInactive from '@assets/registration/progress-bar-2/step-5/step-5-inactive.svg'
+
+import Connector from '@assets/registration/progress-bar-2/progress-bar-connector/inactive.svg'
 
 import { Link } from 'react-router-dom';
 
@@ -8,8 +21,25 @@ export default function IntakeDefault() {
         <div className='py-20 bg-[#345EC9]'>
             <div className='border border-white w-[874px] py-8 mx-auto'>
                 <h1 className='text-white text-5xl font-manrope font-bold text-center'>PRODUCT INTAKE FORM</h1>
-                <img src={Default} alt="Default Progress Bar" width={648} className='mx-auto' />
-                <DefaultStep/>
+                <div className='flex justify-between px-32 items-end py-7'>
+                    <img src={StepOneInactive} alt="Step One Inactive" />
+                    <img src={StepTwoInactive} alt="Step Two Inactive" />
+                    <img src={StepThreeInactive} alt="Step Three Inactive" />
+                    <img src={StepFourInactive} alt="Step Four Inactive" />
+                    <img src={StepFiveInactive} alt="Step Five Inactive" />
+                </div>
+                {/* the below is for the number progress bar */}
+                <div className='flex justify-center pb-3'>
+                    <img src={PageOneInactive} alt="Page One Inactive" />
+                    <img src={Connector} alt="Connector" />
+                    <img src={PageTwoInactive} alt="Page Two Inactive" />
+                    <img src={Connector} alt="Connector" />
+                    <img src={PageThreeInactive} alt="Page Three Inactive" />
+                    <img src={Connector} alt="Connector" />
+                    <img src={PageFourInactive} alt="Page Four Inactive" />
+                    <img src={Connector} alt="Connector" />
+                    <img src={PageFiveInactive} alt="Page Five Inactive" />
+                </div>
                 <div className='flex justify-between px-32'>
                 <p className='text-[#ffffff80] text-sm font-manrope font-normal'>Product Info</p>
                 <p className='text-[#ffffff80] text-sm font-manrope font-normal -ml-10'>Preferred Communication</p>
