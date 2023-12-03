cb84a8974d593c63cc8aac8a30c8ba7dccd43cc4
John Jang <jscolbe9675@gmail.com>
2023-12-03 00:45:20 -0500

    making some changes in the login logic

--- src/components/navbar/default/index.tsx
+++ src/components/navbar/default/index.tsx
@@ -1,78 +1,7 @@
-import { useContext } from "react";
 import { Link } from "react-router-dom";
-import { signOut } from "firebase/auth";
-import { auth } from "@components/api/firebase";
-import { UserContext } from "@contexts/UserContext";
 import BrandLogo from "@assets/brand/brand-logo.svg";
 
-function getDashboardRoute(role: "admin" | "producer" | "evaluator" | null) {
-  if (role) {
-    return role;
-  }
-  // XXX
-  // somehow we got a user with "null" role...what should we do?
-  return "login";
-}
-
-async function signOutUser() {
-  try {
-    await signOut(auth);
-  } catch (err) {
-    // XXX
-    // Handle better
-    console.error(err);
-  }
-}
-
 export default function NavBar() {
-  const user = useContext(UserContext);
-
-  // XXX
-  // i just copied & tweaked the navbar code below for now - sean
-  if (user) {
-    return (
-      <>
-        <div className="flex justify-end bg-black w-full h-10 text-right px-[73px] pt-2 pb-1">
-          <div className="flex gap-6">
-            <Link to={`/${getDashboardRoute(user.role)}`} className="text-white text-xl font-Manrope font-semibold">
-              {/* <Profile user={user}/> */} My Dashboard
-            </Link>
-            <button
-              className="text-white text-xl font-Manropoe font-semibold"
-              onClick={signOutUser}
-              type="button"
-            >
-              Sign Out
-            </button>
-          </div>
-        </div>
-        <nav className="bg-[#0F7B41] w-full h-[88px] py-8 px-14 flex justify-between items-center">
-          <Link to="/">
-            <img alt="Feedback logo" src={BrandLogo} className="shrink-0" />
-          </Link>
-          <div className="flex items-center gap-14">
-            <Link to="/about-us">
-              <button className="text-white text-xl font-semibold font-Manrope">
-                About us
-              </button>
-            </Link>
-            <Link to="/how-it-works">
-              <button className="text-white text-xl font-semibold font-Manrope">
-                How it works
-              </button>
-            </Link>
-            <Link to="/pricing">
-              <button className="text-white text-xl font-semibold font-Manrope">
-                Pricing
-              </button>
-            </Link>
-          </div>
-          <div></div>
-        </nav>
-      </>
-    );
-  }
-
   return (
     <>
       <div className="bg-black w-full h-10 text-right px-[73px] pt-2 pb-1 relative z-[11]">
@@ -104,9 +33,7 @@ export default function NavBar() {
           </Link>
         </div>
         <button className="text-[#0F7B41] text-xl shrink-0 w-48 h-11 rounded-2xl bg-white text-center font-manrope font-bold">
-          <Link to='/registration'>
-          GET STARTED
-          </Link>
+          <Link to="/registration">GET STARTED</Link>
         </button>
       </nav>
     </>
--- src/components/navbar/navbar-2/index.tsx
+++ src/components/navbar/navbar-2/index.tsx
@@ -2,8 +2,30 @@ import BrandLogo from "@assets/brand/brand-logo.svg";
 import Profile from "@components/user-profile";
 import { Link, useLocation } from "react-router-dom";
 
+// Firebase Authentication
+import { signOut } from "firebase/auth";
+import { auth } from "@components/api/firebase";
+
+// User Context
+import { createContext, useContext } from "react";
+
+type User = "producer" | "evaluator" | "admin";
+type UserContextType = {
+  role: User | null;
+};
+
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+const useUserContext = () => {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error("useUserContext must be used within a UserContextProvider");
+  }
+  return context;
+};
+
 export default function Navbar2() {
-  const user = {
+  const userProfile = {
     photoURL: "url-to-user-photo",
     displayName: "User Display Name",
     userRole: "User Role",
@@ -14,13 +36,40 @@ export default function Navbar2() {
   const isProducerDashboard = location.pathname.startsWith("/producer");
   const isEvaluatorDashboard = location.pathname.startsWith("/evaluator");
 
+  const { role } = useUserContext();
+
+  function getDashboardRoute(role: User | null) {
+    if (role) {
+      return `/${role}`;
+    }
+    // Handle the case where role is null (if needed)
+    return "/";
+  }
+
+  async function signOutUser() {
+    try {
+      await signOut(auth);
+    } catch (err) {
+      // XXX
+      // Handle better
+      console.error(err);
+    }
+  }
+
   return (
     <div className="bg-[#D9D9D9] h-full">
       <header>
         <Link to="/">
           <img alt="Feedback logo" src={BrandLogo} className="shrink-0" />
         </Link>
-        <Profile user={user} />
+        <Profile user={userProfile} />
+        <button
+          className={`text-left ${
+            isProducerDashboard || isEvaluatorDashboard ? "active" : ""
+          }`}
+        >
+          <Link to={getDashboardRoute(role)}>Dashboard Home</Link>
+        </button>
       </header>
       <main className="flex flex-col pl-2">
         {/* buttons for Producer Dashboard here */}
@@ -60,6 +109,8 @@ export default function Navbar2() {
           className={`text-left ${
             isProducerDashboard || isEvaluatorDashboard ? "active" : ""
           }`}
+          onClick={signOutUser}
+          type="button"
         >
           Messages
         </button>
