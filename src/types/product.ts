81a19af81f6b34a8e9edca93b208cee483ab4cdc
Sean Eric Thomas <set@seanericthomas.com>
2023-12-12 13:58:33 -0800

    Refactor product type interface

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,8 +1,7 @@
 import Evaluation from "./evaluation";
 
 export default interface Product {
-  // this is just the document id, for reference (optional, since we're letting
-  // firestore create a document id and won't have it until that happens)
+  // this is just the document id, for reference
   productId?: string;
   // uid of the product's producer
   producerId: string;
@@ -10,8 +9,6 @@ export default interface Product {
   evaluatorIds: null | string[];
   evaluations: null | Evaluation[];
   // rudimentary info for experimenting - not fully developed in this phase
-  info: {
-    goal: string;
-    productName: string;
-  };
+  goal: string;
+  productName: string;
 }
