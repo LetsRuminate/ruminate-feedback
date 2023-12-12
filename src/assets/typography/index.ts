d284048998c937a6a1dac3919566610edfb67169
John Jang <jscolbe9675@gmail.com>
2023-12-12 12:23:32 -0500

    made some UI changes in login page

---
+++ src/assets/typography/index.ts
@@ -0,0 +1,47 @@
+// styles.ts
+export const typography = {
+  title: {
+    primary: {
+      "p-h2": {
+        name: "H2/Primary",
+        fontFamily: "manrope",
+        fontWeight: 700,
+        fontSize: "48px",
+      },
+      "p-h3": {
+        name: "H3/Primary",
+        fontFamily: "manrope",
+        fontWeight: 700,
+        fontSize: "24px",
+      },
+      "p-h4": {
+        name: "H4/Primary",
+        fontFamily: "manrope",
+        fontWeight: 700,
+        fontSize: "20px",
+      },
+      "p-h6": {
+        name: "H6/Primary",
+        fontFamily: "manrope",
+        fontWeight: 700,
+        fontSize: "16px",
+      },
+    },
+  },
+  body: {
+    primary: {
+      "p-b2": {
+        name: "B2/Primary",
+        fontFamily: "manrope",
+        fontWeight: 400,
+        fontSize: "16px",
+      },
+    },
+  },
+};
+
+export const HeadlineOne = {
+  fontSize: "48px",
+  fontFamily: "manrope",
+  fontWeight: 700,
+};
