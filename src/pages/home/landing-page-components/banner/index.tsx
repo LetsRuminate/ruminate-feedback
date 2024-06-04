16bd76313f2ea044529ba72709d07b6358367b63
Wildiris5 <108447194+Wildiris5@users.noreply.github.com>
2024-06-04 16:32:08 -0700

    Update index.tsx
    Made changes to update the design. Added some initial code for registration link.

--- src/pages/home/landing-page-components/banner/index.tsx
+++ src/pages/home/landing-page-components/banner/index.tsx
@@ -1,35 +1,38 @@
-// The Banner Part - importing images in .svg
-import RedCircle from '@assets/banner/red-circle/red-circle.svg'
-import PlayIcon from '@assets/banner/red-button/play-icon.svg'
-import Sandwich from '@assets/banner/sandwich/sandwich.svg'
-import RedUnderline from '@assets/banner/red-underline/red-underline.svg'
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import Peppers from '@assets/banner/peppers/image 57.png'
+import BlackArrow from '@assets/banner/arrow/black-arrow.svg'
 
 export default function Banner() {
   return (
     <>
     {/* the below section is for the introductory banner */}
-      <section className="flex bg-[#4670D9] gap-52 -skew-y-6 -mt-20 -z-[10] relative border-b-[24px] border-black justify-center">
-        <div className="pt-36 pb-28 skew-y-6">
-          <h1 className="text-white text-7xl font-anton font-normal mb-4">
-            Making Good Food Better
+      <section className="flex bg-[#790E4A] gap-52 -skew-y-6 -mt-20 -z-[10] relative border-b-[24px] border-black justify-center">
+        <div className="pt-40 pb-28 skew-y-6 ml-[130px] -z-[8] w-full">
+          <h1 className="text-white text-[81px] header1 mb-4 inline">
+            MAKING GOOD FOOD 
           </h1>
-        <div className='flex'>
-          <img src={RedCircle} alt='Red Circle' width={233} className=' h-36 ml-48 -mt-32'/>
-          <img src={RedUnderline} alt="Red Underline" width={169} className='ml-28 -mt-4 mb-5'/>
-        </div>
-        <h2 className="text-[#BDF0FF] text-4xl font-anton font-normal mb-8">
-            THINK LIKE A SCIENTIST. ORGANIZE LIKE AN ACTIVIST.<br/>
+          <h1 className="text-white text-[81px] header2 mb-4 inline ml-6 tracking-widest">
+            BETTER 
+          </h1>
+        <h2 className="text-[#B9EDD6] text-[40px] -mb-4 header1 tracking-wide">
+            THINK LIKE A SCIENTIST. ORGANIZE LIKE AN ACTIVIST.
+        </h2>
+        <h2 className="text-[#B9EDD6] text-[40px] mb-8 header1 tracking-wide">
             MAKE SMALL BUSINESS THRIVE.
         </h2>
         <p className="text-white text-2xl font-manrope font-normal mb-14">
-            Driven by more ethical, equitable, and sustainable foodways,
+            Driven by more ethical, equitable, and sustainable foodways.
         </p>
-        <button className='bg-[#EF4E5D] rounded-full items-center text-center flex py-4 px-7 gap-4'>
-          <span className='text-5xl text-white font-anton font-normal'>GET STARTED</span>
-          <img src={PlayIcon} alt="Play Icon" width={30} className='text-white h-[40px]'/>
+        <Link to="/registration">
+        <button 
+          className="bg-[#F9BCAC] hover:buttonhover rounded-full items-center text-center flex py-4 px-7 gap-4 buttonshadow"> 
+          <span className='text-4xl text-black header1 pt-3'>GET STARTED</span>
+          <img src={BlackArrow} alt="Arrow Icon" width={30}/>
         </button>
+        </Link>
         </div>
-        <img src={Sandwich} alt='Sandwich' width={195} className='mt-36 skew-y-6 h-[480px]'/>
+        <div className='ml-[950px] absolute -z-[9]'><img src={Peppers} alt="Peppers"/></div>
       </section>
     </>
   );
