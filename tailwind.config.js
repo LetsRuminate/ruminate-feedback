51dedb53cc422a14d022c0e5a765487c6740ac0a
Wildiris5 <108447194+Wildiris5@users.noreply.github.com>
2024-06-04 16:49:35 -0700

    Update tailwind.config.js
    Started work on how app will display at different screen sizes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ export default {
     //
   ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'},
     extend: {
       fontFamily: {
         manrope: ["manrope", "sans-serif"],
