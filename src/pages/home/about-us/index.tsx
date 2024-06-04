ddb35a221af0698b3b719a3da5d91977826ce50e
Wildiris5 <108447194+Wildiris5@users.noreply.github.com>
2024-06-04 16:26:42 -0700

    Update index.tsx
    About Us page initial adjustments.

--- src/pages/home/about-us/index.tsx
+++ src/pages/home/about-us/index.tsx
@@ -1,8 +1,34 @@
 export default function AboutUs() {
   return (
-    <div className="py-80 text-center">
-      <p>This is about us page.</p>
-      {/*  */}
-    </div>
+    <>
+    <section>
+      <div className='flex justify-center'>
+      <div className='bg-white mb-10 w-1/2 rounded-3xl -z-[9] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+      <h1 className='text-[#000000] ml-20 mt-10 text-5xl font-anton font-normal mb-4'>About Us</h1>
+      <p className='ml-20 mr-20 mb-20'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+      
+      </div>
+      
+      </section>
+      <section className="flex bg-[#F9BCAC] gap-52 -skew-y-6 -mt-20 -z-[10] relative border-b-[24px] border-black justify-center">
+        <div className="pt-36 pb-48 skew-y-6">
+          
+        <div className='flex'>
+          
+        </div>
+        </div>
+      </section>
+      {/* Second section */}
+      <section className='bg-[#E4DD67] pt-40 pb-52 -mt-20 -z-[11] relative'>
+    
+      
+      
+      </section>
+      
+      
+    </>
   );
 }
